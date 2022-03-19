68af0eb1f7b969c0464385ab378b26892aafc7b0
Volkov Leonid <leonid29_04@mail.ru>
2022-03-19 16:58:14 +0300

    Initial Commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
+    private lateinit var diceImage: ImageView
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,8 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6)+1
-        val drawableResource = when (randomInt){
+        val drawableResource = when (Random().nextInt(6)+1){
             1->R.drawable.dice_1
             2->R.drawable.dice_2
             3->R.drawable.dice_3
